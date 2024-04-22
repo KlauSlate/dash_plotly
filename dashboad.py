1167bcc26813a126e23185b92cfe4ce6f456ca36
KlauSlate <cesargarridoescobar@gmail.com>
2024-04-21 23:08:42 -0400

    se modifica el archivo dash y se agrega el archivo xlsx

--- dashboad.py
+++ dashboad.py
@@ -4,77 +4,60 @@ from dash import dcc, html
 from dash.dependencies import Input, Output
 import plotly.graph_objects as go
 import pandas as pd
+import plotly.express as px
 
 # cargar los datos desde el archivos .csv
-df = pd.read_csv('Unicorn_Clean.csv')
+df = pd.read_excel('unicornio_startup.xlsx')
 
-def count_non_empty_investors(row):
-    non_empty_count = sum([1 for investor in row if pd.notna(investor)])
-    return non_empty_count
-
-df['non_empty_investors'] = df[['Investor 1', 'Investor 2', 'Investor 3', 'Investor 4']].apply(count_non_empty_investors,
-                                                                                                axis=1)
-
-# crear la aplicacion Dash
+# crear la aplicacion de Dash
 app = dash.Dash(__name__)
 
-# definir el diseñoo de la aplicacion Dash
+# obtener las opciones unicas de la columna 'industry'
+industries = [{'label':industry, 'value':industry} for industry in df['Industry'].unique()]
+
+# definir el diseño de la aplicacion
 app.layout = html.Div([
-    html.H1('Dashboard Startup Unicornios'),
-
-    html.Div([
-        # grafico de treemap
-        dcc.Graph(id='treemap', className='six columns'),
-
-        # grafico heatmap
-        dcc.Graph(id='heatmap', className='six columns')
-    ], className='row')
+    html.H1('Dashboard de Startup Unicornios'),
+    html.Label('Selecciona la industria para visualizar el grafico:'),
+    dcc.Dropdown(
+        id='dropdown',
+        options=industries,
+        value=industries[0]['value']
+    ),
+    dcc.Graph(id='treemap'),
+    html.Hr(), # linea horizontal para separar los graficos
+    html.H2('Heatmap de N° de inversores por industria'),
+    dcc.Graph(id='heatmap')
 ])
 
-# funcion de callback para actualizar el grafico treemap
+# definir la interactividad del treemap
 @app.callback(
-    [Output('treemap', 'figure'),
-    Output('heatmap', 'figure')],
-    [Input('treemap', 'clickData')]
+    Output('treemap','figure'),
+    [Input('dropdown','value')]
 )
 
-def update_graph(clickData):
-    if clickData is None:
-        selected_category = 'Categoria Predeterminada' # define una categoria predeterminada
-    else:
-        selected_category = clickData['points'][0]['label']
+def update_treemap(selected_value):
+    filtered_df = df[df['Industry'] == selected_value]
+    fig = px.treemap(filtered_df, path=[px.Constant('Industry'),'Country','Company'],
+                    values='Valuation ($B)',
+                    title=f'Treemap de valorizacion para {selected_value}')
+    return fig
 
-    # filtrar el dataframe por la categoria seleccionada
-    filtered_df = df[df['Industry'] == selected_category]
+# definir la interactividad del heatmap
+@app.callback(
+    Output('heatmap','figure'),
+    [Input('dropdown','value')]
+)
 
-    # calcular el numero de inversores por pais para la categoria seleccionada
-    inverstors_by_country = filtered_df.groupby('Country')['non_empty_investors'].sum().reset_index()
-
-    # crear el treemap
-    trace_treemap = go.Treemap(
-        labels=filtered_df['Country'],
-        parents=[''] * len(filtered_df),
-        values=filtered_df['non_empty_investors']
-    )
-    layout_treemap = go.Layout(title=f'Distribucion de Inversores para la industria {selected_category}')
-    fig_treemap = {'data': [trace_treemap], 'layout': layout_treemap}
-
-    # crear el heatmap
-    trace_heatmap = go.Heatmap(
-        x=inverstors_by_country['Country'],
-        y=['N° Inversores'],
-        z=[inverstors_by_country['non_empty_investors']],
-        colorscale= 'Viridis',
-        colorbar=dict(title='N° de Inversores')
-    )
-    layout_heatmap = go.Layout(
-        title='Numero de Inversores por Pais',
-        xaxis=dict(title='Pais'),
-        yaxis=dict(title=''),
-        height=500
-    )
-    fig_heatmap = {'data': [trace_heatmap], 'layout': layout_heatmap}
-    return fig_treemap, fig_heatmap
+def update_figure(selected_value):
+    filtered_df = df[df['Industry'] == selected_value]
+    heatmap_data = filtered_df.groupby(['Industry', 'Country']).size().unstack(fill_value=0)
+    fig = px.imshow(heatmap_data.values,
+                    labels=dict(x="Country", y="Industry", color="Count"),
+                    x=list(heatmap_data.columns),
+                    y=list(heatmap_data.index),
+                    title="Heatmap de Numeros de inversore por Industria")
+    return fig
 
 # ejecutar la aplicacion
 if __name__ == '__main__':
