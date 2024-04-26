f3ca876bc72f242d20e444a1c09eda2efc46c9ba
Nicolas Bustos <nicolas.bustos@timining.com>
2024-04-26 16:29:49 -0400

    filtra los 10 mayores inversores por industria en el cuarto gráfico

--- dashboad.py
+++ dashboad.py
@@ -132,8 +132,13 @@ def update_total_valuation(selected_value):
     [Input('dropdown', 'value')]
 )
 def update_investor1_histogram(selected_value):
-    filtered_df = df[df['Industry'] == selected_value]
-    fig = px.histogram(filtered_df, x='Investor 1', title="Histograma de Investor 1")
+    df_filtered = df[df['Industry'] == selected_value]
+    investor_per_industry = [df_filtered[["Industry", investor]].rename(lambda x: x.split()[0], axis=1) for investor in
+                             df.columns if investor.startswith("Investor")]
+    investors = pd.concat(investor_per_industry).dropna()
+    top10 = investors.Investor.value_counts().nlargest(10).index.tolist()
+    investors_filtered = investors[investors.Investor.isin(top10)]
+    fig = px.histogram(investors_filtered, x='Investor', title=f"Número de startups por inversor TOP 10 {selected_value}")
     return fig
 
 # Definir la interactividad del coleoptero
